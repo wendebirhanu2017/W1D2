26ae86fa65b969c39834efa4b3cc4c094cd29dac
Wendmagegn Kebede <wendmagegn.b@gmail.com>
2022-10-04 20:55:56 -0500

    Event Listener Completed

--- app/src/main/java/com/example/w1d2/MainActivity.kt
+++ app/src/main/java/com/example/w1d2/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.w1d2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var foodList = ArrayList(listOf("Hamburger", "Pizza", "Mexican", "American", "Chinese"))
+
+        decideBtn.setOnClickListener{
+            var randomNumber = Random.nextInt(0,foodList.size)
+            randText.text = foodList[randomNumber]
+        }
+
+        addBtn.setOnClickListener{
+            var value: String?= edit.text.toString()
+            if(value?.length!! >0) {
+                foodList.add(edit.text.toString())
+                edit.text.clear()
+            }
+        }
     }
 }
